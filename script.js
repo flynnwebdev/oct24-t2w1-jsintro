338fca711b4e2fd1fd814b656102ce06ff7f9de4
Mr. Robot <flynnwebdev@gmail.com>
2025-03-31 20:03:11 +1000

    Finished intro JS lesson

---
+++ script.js
@@ -0,0 +1,47 @@
+a = 5 // global
+console.log(a)
+
+var b = 6  // function scope
+console.log(b)
+
+let c = 7 // block scope
+console.log(c)
+
+const d = 8
+// d = 10
+console.log(d)
+
+// Input and output
+// let value = prompt('What is your name?')
+
+// Template string (like a Python f-string)
+// print(f"Your name is {name}")
+// console.log(`Your name is ${value}`)
+
+// Data Types
+
+// Boolean
+true
+false
+
+// Falsy values
+console.log(Boolean(false))
+console.log(Boolean(0))
+console.log(Boolean(""))
+console.log(Boolean(NaN)) // Not A Number
+console.log(Boolean(null)) // None in Python
+console.log(Boolean(undefined))
+
+// Typecast
+// int(2.5)
+
+// Truthy values
+console.log(Boolean(12))
+console.log(Boolean('Hi'))
+console.log(Boolean({}))
+
+// Number
+// type(2.5) # returns float
+console.log(typeof('Hi'))
+console.log(typeof(2))
+console.log(typeof('Hi'))
