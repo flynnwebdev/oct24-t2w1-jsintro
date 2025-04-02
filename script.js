41539a53d8120c361d4868e6ceaac3b7b7e5d5df
Mr. Robot <flynnwebdev@gmail.com>
2025-04-02 19:35:41 +1000

    Finished JS intro

--- script.js
+++ script.js
@@ -45,3 +45,53 @@ console.log(Boolean({}))
 console.log(typeof('Hi'))
 console.log(typeof(2))
 console.log(typeof('Hi'))
+
+console.log(1/2)
+console.log(1/"2") // Type coercion
+console.log(1/0)
+console.log(1/"hi")
+
+// Explicit cast
+x = "2"
+console.log(1/Number(x))
+
+// Array (list in Python)
+const names = ['John', 'Jane', 'Bob']
+const foo = ['Matt', 52, 184.5]
+console.log(names[1])
+names.push('Mary', 'Jason')
+console.log(names)
+const name = names.pop()
+console.log(names)
+// Python naming convention is snake case - big_array
+const bigArray = names.concat(foo)  // Camel case
+console.log(bigArray)
+console.log(names)
+
+// Objects - dict in Python
+// Object literal
+const person1 = {
+    // Python: "name": "John"
+    name: "John",
+    age: 32,
+    city: "Sydney"
+}
+
+console.log(person1.age)
+// Object.freeze(person1)
+person1.country = 'Australia'
+// person1.city = 'Melbourne'
+console.log(person1)
+
+const person2 = {
+    name: 'Sally',
+    age: 45,
+    address: {
+        city: 'Melbourne',
+        country: 'Australia'
+    }
+}
+
+// const attr = prompt('Which attribute?')
+// Optional chaining with ?
+console.log(person1.address?.country)
