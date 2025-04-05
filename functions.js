e6826922c72395d6444ee2d45a1acfce71b7a18e
Mr. Robot <flynnwebdev@gmail.com>
2025-04-05 10:32:04 +1000

    Finished functions lesson

---
+++ functions.js
@@ -0,0 +1,15 @@
+// Python
+// def add(x, y):
+//     return x + y
+//
+// lambda (x, y): x + y
+
+const add = (x, y) => x + y
+const square = x => x ** 2
+
+console.log(add(10, 34))
+console.log(square(5))
+
+numbers = [1, 2, 3, 4, 5]
+const result = numbers.map(x => x ** 3)
+console.log(result)
